556b07f3f7811a027f566b925a8902e2da6a0951
minhyeongs <162958572+minhyeongs@users.noreply.github.com>
2024-04-14 09:13:51 +0900

    Update tr2.py

--- tr2.py
+++ tr2.py
@@ -1,9 +1,6 @@
 import discord
 from deepl import Translator as DeeplTranslator
 
-# 디스코드 봇 토큰
-TOKEN = 'MTIxNzMxNjkzNjY2OTU5NzczNg.Gfc7bs.GrsAxUPR65dgARoyZncgtvzDb55D7iWqmbrjQM'
-
 # Deepl API 키
 DEEPL_API_KEY = '6eb802fa-ae94-4f9e-8c72-72d2948fed71'
 
@@ -17,7 +14,7 @@ client = discord.Client(intents=intents)
 # 봇이 작동하기 시작할 때 실행되는 이벤트
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game(name="번역할 준비"))
+    await client.change_presence(activity=discord.Game(name="번역 봇입니다"))
     print(f'{client.user}로 로그인했습니다.')
 
 # 메시지가 전송될 때마다 실행되는 이벤트
@@ -36,7 +33,7 @@ async def on_message(message):
     
     # 메시지 번역
     translations = {}
-    languages = {'KO': 'KR', 'ES': 'ES', 'EN-US': 'EN'}
+    languages = {'KO': '한국어', 'ES': '스페인어', 'EN-US': '영어'}
     for lang, lang_name in languages.items():
         translated_message = deepl_translator.translate_text(
             message.content, target_lang=lang
@@ -44,7 +41,7 @@ async def on_message(message):
         translations[lang_name] = translated_message
     
     # 임베드 생성
-    embed = discord.Embed(title="The translation bot has come to translate the message!", description="", color=0x87CEEB)
+    embed = discord.Embed(title="번역 결과", description="", color=0x00ff00)
     for lang_name, translated_message in translations.items():
         embed.add_field(name=lang_name, value=translated_message, inline=False)
     
@@ -52,4 +49,4 @@ async def on_message(message):
     await message.channel.send(embed=embed)
 
 # 봇을 실행
-client.run(TOKEN)
+client.run('MTIxNzMxNjkzNjY2OTU5NzczNg.GOlHir.H6iWtoNz3aro_PoZxCeBcgHTgXT6oNHmYnWAPs')
